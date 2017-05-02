11c741bf1f04a73c8474a82ac026d972844063d4
zol <zol@zol-PC>
2017-05-02 21:07:57 +0800

    豆腐干豆腐给多个

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -5,10 +5,10 @@ class BaseController extends Controller {
 
     public function __construct(){
         $userid = $_COOKIE['userid'];
-        if (!$userid) {
+        /*if (!$userid) {
             //调到授权页面
             echo '跳到授权页面';
             die;
-        }
+        }*/
     }
 }
